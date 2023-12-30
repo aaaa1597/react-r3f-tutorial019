3cf8343337dc9f14a0b7146f94b478fbb4f86787
jun <jun@Ubt2200.aaaaa>
2023-12-30 10:39:43 +0900

    ビルドエラーはとれたけど、イベントに発火しないんだよな〜。なんでだろ。

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import React, {useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 import { Canvas, useFrame, MeshProps  } from '@react-three/fiber'
 import * as THREE from 'three'
-import { OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls } from '@react-three/drei'
+
+const useKeyboard = () => {
+  type typekeyIndexPair = {[key: string]: number }
+  const keyIndexPair: typekeyIndexPair = {'KeyA': 0, 'KeyD': 1,'KeyW': 2,'KeyS': 3,}
+  const keyMap = useRef<boolean[]>([false,false,false,false])
+  // Object.keys(keyIndexPair).forEach((key, value) => {
+  //   console.log("key=" + key, " value=" + value, " keyIndexPair[xxx]=" + keyIndexPair[key]);
+  // });
+
+  useEffect(() => {
+    const onDocumentKey = (e: KeyboardEvent) => {
+      keyMap.current[keyIndexPair[e.code]]! = e.type === 'keydown'
+    }
+    document.addEventListener('keydown', onDocumentKey)
+    document.addEventListener('keyup', onDocumentKey)
+    return () => {
+      document.removeEventListener('keydown', onDocumentKey)
+      document.removeEventListener('keyup', onDocumentKey)
+    }
+  }, [])
+
+  return keyMap.current
+}
 
 const Box = (props: MeshProps) => {
   const ref = useRef<THREE.Mesh>(null!)
+  const keyMap = useKeyboard()
 
   useFrame((_, delta) => {
-    if( !ref.current) return
-    ref.current.rotation.x += 1 * delta
-    ref.current.rotation.y += 0.5 * delta
+    keyMap[0/*KeyA*/] && (ref.current.position.x -=1 * delta)
+    keyMap[1/*KeyD*/] && (ref.current.position.x +=1 * delta)
+    keyMap[2/*KeyW*/] && (ref.current.position.z -=1 * delta)
+    keyMap[3/*KeyS*/] && (ref.current.position.z +=1 * delta)
   })
 
   return (
@@ -25,12 +50,12 @@ const App = () => {
   return (
     <div style={{ width: "75vw", height: "75vh" }}>
       <Canvas camera={{ position: [3, 1, 2] }}>
-        <ambientLight />
-        <directionalLight />
         <Box position={[-0.75, 0, 0]} name="A" />
         <Box position={[0.75, 0, 0]} name="B" />
         <OrbitControls />
-        <gridHelper rotation={[Math.PI / 4, 0, 0]} />
+        <gridHelper />
+        <axesHelper args={[5]} />
+        <Stats />
       </Canvas>
     </div>
   );
